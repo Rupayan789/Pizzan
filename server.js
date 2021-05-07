d788bc4beb2152df2f6f9700e5ed9cff97b0dd62
Rupayan <princerupayan1@gmail.com>
2021-05-07 12:18:33 +0530

    Added login register page

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ const PORT=3000 || process.env.PORT;
 //assets
 app.use(express.static(__dirname+'/public'))
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+
 app.use(expressLayouts);
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 app.listen(PORT,()=>console.log("Server running"));
